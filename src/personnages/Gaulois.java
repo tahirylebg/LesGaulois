a0360e690ce58d4b754aed50414c5c5a2f7f0d3d
tahirylebg <79042708+tahirylebg@users.noreply.github.com>
2022-10-07 08:38:46 +0200

    Début TP1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,11 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetPotion=1;
-	
-	public Gaulois() {
-		//TODO Auto-generated method stub
-	}
+	private int effetPotion=1
 
 	public Gaulois(String nom, int force) {
 		super();
@@ -15,16 +11,22 @@ public class Gaulois {
 		this.force = force;
 	}
 	
-	public String getNom() {
-		return nom;
-	}
-
-	@Override
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+		}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+		}
+	
+	@Override	
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-
+		}
+	
 	public static void main(String[] args) {
-		
-	}
+		//TODO créer un main permettant de tester la classe Gaulois
+		}
+
 }
