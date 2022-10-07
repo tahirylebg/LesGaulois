214340a4c162721e8799b5b9ec235c1b4103dff0
tahirylebg <79042708+tahirylebg@users.noreply.github.com>
2022-10-07 08:43:25 +0200

    debut TP1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,7 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetPotion=1
+	private int effetPotion=1;
 
 	public Gaulois(String nom, int force) {
 		super();
